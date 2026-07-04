aa1d5a3a4770af5f921aa8c74a3208955f2022bd
Andrew <andrey122f@mail.ru>
2026-07-04 21:46:55 +0400

    Начальное меню игры

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ let context = canvas.getContext('2d');
 let bg = new Image();
 let runner = new Image();
 
+// Элементы меню
+const startScreen = document.getElementById('start-menu');
+const nicknameInput = document.getElementById('nickname');
+const startBtn = document.getElementById('start-btn');
+
 bg.src = "img/fon.png";
 runner.src = "img/person.png";
 
@@ -9,7 +14,8 @@ let step_shag = 18;
 let step_jump = 20;
 let bgOffset = 0;
 let mapLength = canvas.width *7;
-
+let gameRunning = false;          // флаг: запущена ли игра
+let playerNickname = 'Игрок';    // ник по умолчанию
 
 let person = {
     x_padding: 0,
@@ -23,6 +29,24 @@ let person = {
 
 let keys = {};
 
+// Включаем/выключаем кнопку в зависимости от того, есть ли ник
+nicknameInput.addEventListener('input', () => {
+  const nick = nicknameInput.value.trim();
+  startBtn.disabled = nick.length <= 2;
+});
+
+// Кнопка СТАРТ
+startBtn.addEventListener('click', () => {
+    const nick = nicknameInput.value.trim();
+    if (nick) {
+        playerNickname = nick;
+    }
+    startScreen.style.display = 'none';
+    gameRunning = true;
+})
+nicknameInput.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') startBtn.click();
+});
 
 bg.onload = function(){
     fondraw();
@@ -36,6 +60,11 @@ function fondraw(){
 }
 
 function tick(){
+    if (!gameRunning) {
+        requestAnimationFrame(tick);
+        return;
+    }
+
     if(person.tick_count > 5){
         spriteRunner();
         person.tick_count = 0;
