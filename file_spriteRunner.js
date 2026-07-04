032b7d4d40e7f7d2f0985011f02127b65e8ec125
Fl1ntex <shirshov.nickitka@yandex.ru>
2026-07-05 01:51:20 +0400

    прыжок отделньной функцией

--- file_spriteRunner.js
+++ file_spriteRunner.js
@@ -62,17 +62,7 @@ function spriteRunner(){
     
 
     if (person.isJamp) {
-        person.jumpTick +=1;
-        
-        if (person.jumpTick <= 4) {
-            person.y_pos -= step_jump;  
-        } else if (person.jumpTick <= 8) {
-            person.y_pos += step_jump;  
-        } else {
-            person.isJamp = false;
-            person.y_pos = 350;
-            person.jumpTick = 0;
-        }
+        jumpFunction();
     }
 
     if (keys['KeyA'] || keys['KeyD']) {
